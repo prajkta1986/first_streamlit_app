d045ea2602b37afba2a58b02441780ed96a7d45e
Prajkta Nanaware <114015633+prajkta1986@users.noreply.github.com>
2022-09-21 09:01:33 -0500

    Update streamlit_app.py
    corrected error at mom's

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's new healthy Diner')
+streamlit.title('My Moms new healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
