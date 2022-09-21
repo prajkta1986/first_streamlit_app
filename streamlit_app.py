995ce3ba0768d69fd87be9b672e796dfc4c7f08a
Prajkta Nanaware <114015633+prajkta1986@users.noreply.github.com>
2022-09-21 09:19:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 
 streamlit.title('My Moms new healthy Diner')
@@ -10,6 +11,6 @@ streamlit.text('🍛 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
                 
 streamlit.header('🍌🥭Build your own fruit smoothie 🍐🍉')
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.daraframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
