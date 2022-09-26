aabb0a67a1ab01f9b98301ae0c1622cf401156db
Prajkta Nanaware <114015633+prajkta1986@users.noreply.github.com>
2022-09-26 14:13:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #my_cur = my_cnx.cursor()
@@ -83,7 +84,8 @@ if streamlit.button('Get Fruit Load List'):
 #Allow the end user to add fruit to the list
 def inser_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+          my_cur.execute("insert into fruit_load_list values (+new_fruit)") 
          return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
@@ -93,7 +95,7 @@ if streamlit.button('Add a Fruit to the List'):
     streamlit.text(back_from_function)
   
 
-add_my_fruit = streamlit.text_input('Which fruit would you like to add?')
+#add_my_fruit = streamlit.text_input('Which fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('fro')")
