4faa9f5d09f59bbb83c9ad231fdf7563ca0c6afd
Prajkta Nanaware <114015633+prajkta1986@users.noreply.github.com>
2022-09-26 13:45:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 streamlit.stop()
 #my_cur = my_cnx.cursor()
 
